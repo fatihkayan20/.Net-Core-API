7bb3d468d9f7d97eddb958c4359be64ad638aadd
fatihkayan20 <fatih.kayan83@gmail.com>
2021-02-18 19:19:02 +0300

    18.02.2021 19:20

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductListed = "Ürünler listelendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Bakım zamanı lütfen daha sonra tekrar deneyin.";
+        public static string UnitPriceInvalid = "Ürün fiyatı 0 olamaz.";
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {            
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
